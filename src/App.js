70195fa4c0df08f97496244af866aba0332b3a5a
Ally-3 <146335458+Ally-3@users.noreply.github.com>
2023-11-02 15:37:20 +0000

    imports added to app.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
-import cat from './Components/cat';
+import Cats from './Components/Cats';
 import RandomCat from './Components/faker';
 import './App.css';
+import Landing from './Components/Landing';
+import Navbar from './Components/Navbar';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 function App() {
   return (
-    <div>
-      <cat />
+    <div className="App">
+      <Navbar />
+      {/* Remember to import:
+      import { BrowserRouter, Route, Routes } from 'react-router-dom'; */}
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Landing />} />
+        </Routes>
+      </BrowserRouter>
       <RandomCat />
     </div>
   );
