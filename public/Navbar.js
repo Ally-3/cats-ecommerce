71e717593591a827569c01c3a103d5b0761e3ba7
Mark Adams <MAAdams93@googlemail.com>
2023-11-02 12:07:34 +0000

    Navbar

---
+++ public/Navbar.js
@@ -0,0 +1,11 @@
+import { Wrapper, NavbarItem } from './Navbar.styles';
+const Navbar = () => {
+ return (
+ <Wrapper>
+ <NavbarItem>Home</NavbarItem>
+ <NavbarItem>About</NavbarItem>
+ <NavbarItem>Contact</NavbarItem>
+ </Wrapper>
+ );
+};
+export default Navbar
