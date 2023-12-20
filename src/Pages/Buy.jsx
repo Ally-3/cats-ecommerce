529aa3e3a09e4d89a3f7ce49c499f768dc312bbe
Ally-3 <146335458+Ally-3@users.noreply.github.com>
2023-12-20 15:56:57 +0000

    buyCats adjusted to stop cat info from re-rendering when buy button is clicked, values stored in components state

--- src/Pages/Buy.jsx
+++ src/Pages/Buy.jsx
@@ -1,11 +1,10 @@
 import React, { useState, useEffect } from 'react';
 import { faker } from '@faker-js/faker';
 import '../Components/cat.css';
-// import CatImages from '../Components/CatImages'; //CatImages no longer necessary
 
 const BuyCats = (props) => {
   const [cats, setCats] = useState([]);
-  const [imageUrl, setImageUrl] = useState([]);
+  const [catData, setCatData] = useState([]);
 
   useEffect(() => {
     const url = `https://api.thecatapi.com/v1/breeds`;
@@ -19,12 +18,32 @@ const BuyCats = (props) => {
       .then((response) => response.json())
       .then((data) => {
         setCats(data);
-        
       })
       .catch((error) => {
         console.log(error);
       });
   }, []); // Empty dependency array to ensure the effect runs only once
+
+  // Generate random cat data once and store them in the component's state
+  // this way stops a re-render that generates new values
+  // the buy button uses data from catData when clicked
+  useEffect(() => {
+    if (cats.length > 0) {
+      const generatedCatData = Array.from({ length: 12 }).map(() => {
+        const randomCat = cats[Math.floor(Math.random() * cats.length)];
+
+        // Additional checks to ensure necessary properties are present
+        const imageUrl = randomCat?.image?.url || '';
+        const name = faker.person.firstName();
+        const price = faker.commerce.price();
+        const breed = faker.animal.cat();
+
+        return { name, price, breed, imageUrl };
+      });
+
+      setCatData(generatedCatData);
+    }
+  }, [cats]);
 
   let clickBuy = (event, { name, breed, price, imageUrl }) => {
     console.log('Buy button clicked!');
@@ -37,36 +56,29 @@ const BuyCats = (props) => {
 
   return (
     <div className="cat-parent">
-      {Array.from({ length: 12 }).map((_, index) => {
-        const name = faker.person.firstName();
-        const price = faker.commerce.price();
-        const breed = faker.animal.cat();
-        const imageUrl = cats.length > 0 ? cats[index % cats.length].image.url : '';
-
-        return (
-          <div key={index} className="cat-child">
-            <div className="cat-image">
-                {imageUrl ? (
-                <img id="image-cat" className="cat-image" alt="Cat" src={imageUrl} />
-                ) : (
-                <div className="cat-image">No cat images available</div>
-                )}
-            </div>
-            <div>
-              <h3 className="cat-name">Name: {name} </h3>
-              <h3 className="cat-breed">Breed: {breed}</h3>
-              <h3 className="cat-price">Price: £{price}</h3>
-              <button
-                className="buy-button"
-                data-item-price={price}
-                onClick={(event) => clickBuy(event, { name, breed, price, imageUrl })}
-              >
-                Buy now
-              </button>
-            </div>
+      {catData.map((cat, index) => (
+        <div key={index} className="cat-child">
+          <div className="cat-image">
+            {cat.imageUrl ? (
+              <img id="image-cat" className="cat-image" alt="Cat" src={cat.imageUrl} />
+            ) : (
+              <div className="cat-image">No cat images available</div>
+            )}
           </div>
-        );
-      })}
+          <div>
+            <h3 className="cat-name">Name: {cat.name} </h3>
+            <h3 className="cat-breed">Breed: {cat.breed}</h3>
+            <h3 className="cat-price">Price: £{cat.price}</h3>
+            <button
+              className="buy-button"
+              data-item-price={cat.price}
+              onClick={(event) => clickBuy(event, cat)}
+            >
+              Buy now
+            </button>
+          </div>
+        </div>
+      ))}
     </div>
   );
 };
